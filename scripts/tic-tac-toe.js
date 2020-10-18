1b662eff8559f8f5228333b25647ba5d18a9591a
pschuber <pschuber@umich.edu>
2020-10-17 22:31:33 -0400

    Update tic-tac-toe.js

--- scripts/tic-tac-toe.js
+++ scripts/tic-tac-toe.js
@@ -1,38 +1,43 @@
-
-
-
-
 class human {
-constructor(type){
-    if(type===0){
-        this.x_or_o="O";
-    }
-    else{
-        this.x_or_o="X";
-    }
+    constructor(type){
+        if(type===0){
+         this.x_or_o="O";
+        }
+        else{
+            this.x_or_o="X";
+        }
 }
 
 }
 class computer {
-constructor(type){
-    if(type===0){
-        this.x_or_o="O";
-    }
-    else{
-        this.x_or_o="X";
-    }
+    constructor(type){
+        if(type===0){
+          this.x_or_o="O";
+        }
+        else{
+            this.x_or_o="X";
+        }
   
-}
+    }
 
 }
 class Game{
-constructor(){
-    this.game_in_progress =false;
-    this.game_over=false;
-    this.players= new Array();
+    constructor(){
+        this.game_in_progress =false;
+        this.game_over=false;
+        this.players= new Array();
+        
 
-}
-
+    }
+    removeAttributes(){
+        var blocks = this.grid.getElementsByTagName('p');
+        for (let index = 0; index < blocks.length; index++) {
+            blocks[index].removeEventListener('click',color_change);
+            if(blocks[index].hasAttribute('class')){
+                blocks[index].removeAttribute('class');
+            }
+        }
+    }
 
 }
 let mygame = new Game();
@@ -48,8 +53,9 @@ function color_change(evt){
 function start_reset(evt){
    
     if(evt.target.value==="Start!"){
-    let human_v_human = document.getElementById('human_human');
-    let computer_v_human = document.getElementById('human_computer');
+        let human_v_human = document.getElementById('human_human');
+        let computer_v_human = document.getElementById('human_computer');
+
         if(!human_v_human.checked && !computer_v_human.checked){
             alert("Please Select a game mode!");
         }
@@ -89,10 +95,7 @@ function start_reset(evt){
     }
     else if(evt.target.value==="Restart!"){
         mygame.game_in_progress=false;
-        const blocks=document.getElementById('grid').getElementsByTagName('p');
-        for (let index = 0; index < blocks.length; index++) {
-            blocks[index].removeEventListener('click',color_change);
-        }
+        mygame.removeAttributes();
        evt.target.value="Start!"
     }
 
@@ -100,7 +103,7 @@ function start_reset(evt){
 
 
 window.addEventListener('load', function(){
-
+    mygame.grid= document.getElementById('grid');
     document.getElementById('start_reset').addEventListener('click', start_reset);
 
     
